9639703ef5652fb8d862432eb64bc7b5f53f5909
Yashvanthk05 <yashvanthk05@gmail.com>
2025-02-08 15:41:41 +0530

    page simple

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 "use client"
 import { redirect } from 'next/navigation';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 
 export default function Home() {
   redirect("/materials");
